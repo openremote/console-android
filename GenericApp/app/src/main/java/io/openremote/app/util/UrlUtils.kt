80a05b7f736269be14444aff3ca1c32e26a34bc1
Eric Bariaux <375613+ebariaux@users.noreply.github.com>
2025-04-15 16:00:10 +0200

    Extract function to create Url from host (domain), add tests and implement logic similar to iOS

--- GenericApp/app/src/main/java/io/openremote/app/util/UrlUtils.kt
+++ GenericApp/app/src/main/java/io/openremote/app/util/UrlUtils.kt
@@ -1,5 +1,7 @@
 package io.openremote.app.util
 
+import android.webkit.URLUtil
+
 object UrlUtils {
     fun isIpAddress(url: String): Boolean {
         val ipPattern = Regex(
@@ -7,8 +9,21 @@ object UrlUtils {
         )
         return ipPattern.matches(url)
     }
+
+    fun isIpV6NoScheme(url: String): Boolean {
+        val ipv6Pattern = Regex(
+            "^(?:([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6}))$"
+        )
+        return ipv6Pattern.matches(url)
+    }
+
     fun startsWithHttp(url: String): Boolean {
         return url.startsWith("http://") || url.startsWith("https://")
+    }
+
+    fun startsWithScheme(url: String): Boolean {
+        val schemePattern = Regex("^[a-zA-Z]+://.*$")
+        return schemePattern.matches(url)
     }
 
     fun endsWithTld(url: String): Boolean {
@@ -16,5 +31,15 @@ object UrlUtils {
             "(?:[a-zA-Z]*\\.)+([a-zA-Z]+)(?:\\/.*)?"
         )
         return tldPattern.matches(url)
+    }
+
+    fun hostToUrl(host: String): String {
+        return when {
+            isIpV6NoScheme(host) -> "https://[${host}]"
+            startsWithScheme(host) ->
+                if (host.contains(".") || host.contains("[")) host else "${host}.openremote.app"
+            (host.contains(".") || host.contains("[")) -> "https://${host}"
+            else -> "https://${host}.openremote.app"
+        }
     }
 }
