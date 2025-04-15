4e62d71dc556915f61b14313cf7ac4d63dfff121
Eric Bariaux <375613+ebariaux@users.noreply.github.com>
2025-04-15 16:03:29 +0200

    Remove unused functions

--- GenericApp/app/src/main/java/io/openremote/app/util/UrlUtils.kt
+++ GenericApp/app/src/main/java/io/openremote/app/util/UrlUtils.kt
@@ -1,15 +1,6 @@
 package io.openremote.app.util
 
-import android.webkit.URLUtil
-
 object UrlUtils {
-    fun isIpAddress(url: String): Boolean {
-        val ipPattern = Regex(
-            "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(:\\d{1,5})?$"
-        )
-        return ipPattern.matches(url)
-    }
-
     fun isIpV6NoScheme(url: String): Boolean {
         val ipv6Pattern = Regex(
             "^(?:([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6}))$"
@@ -17,20 +8,9 @@ object UrlUtils {
         return ipv6Pattern.matches(url)
     }
 
-    fun startsWithHttp(url: String): Boolean {
-        return url.startsWith("http://") || url.startsWith("https://")
-    }
-
     fun startsWithScheme(url: String): Boolean {
         val schemePattern = Regex("^[a-zA-Z]+://.*$")
         return schemePattern.matches(url)
-    }
-
-    fun endsWithTld(url: String): Boolean {
-        val tldPattern = Regex(
-            "(?:[a-zA-Z]*\\.)+([a-zA-Z]+)(?:\\/.*)?"
-        )
-        return tldPattern.matches(url)
     }
 
     fun hostToUrl(host: String): String {
