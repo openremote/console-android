8c44629297d30450fa3b2ba0ca5766453a878f3f
Eric Bariaux <375613+ebariaux@users.noreply.github.com>
2025-04-15 16:01:29 +0200

    Was using Roboelectric because original implementation used function from Android SDK, not the case anymore

--- GenericApp/app/src/test/java/io/openremote/app/util/UrlUtilsTest.kt
+++ GenericApp/app/src/test/java/io/openremote/app/util/UrlUtilsTest.kt
@@ -2,12 +2,6 @@ import io.openremote.app.util.UrlUtils
 import org.junit.Test
 import org.junit.Assert.assertEquals
 
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
-
-@RunWith(RobolectricTestRunner::class)
-@Config(sdk = [33], manifest=Config.NONE)
 class UrlUtilsTest {
 
     @Test
